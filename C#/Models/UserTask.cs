f2cca3c289dc26fc3f4509fa1325b950fbff449a
dirk.pixel <schaarschmidt.dirk@gmail.com>
2025-06-27 04:02:17 +0200

    Add files via upload

--- C#/Models/UserTask.cs
+++ C#/Models/UserTask.cs
@@ -1,1 +1,33 @@
+﻿namespace NexusApp.Models
+{
+    /// <summary>
+    /// Definiert die verschiedenen Arten von Aufgaben, die ausgeführt werden können.
+    /// Die Verwendung eines Enums anstelle von Strings verhindert Tippfehler.
+    /// </summary>
+    public enum TaskProcessType
+    {
+        Einstellung,
+        Versetzung,
+        Austritt
+    }
 
+    /// <summary>
+    /// Repräsentiert eine einzelne Aufgabe, die für einen Benutzer ausgeführt werden soll.
+    /// Diese Klasse dient als Datenmodell für die gesamte Anwendung.
+    /// </summary>
+    public class UserTask
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public TaskProcessType Type { get; set; }
+
+        // KORRIGIERT: Dieser Konstruktor stellt sicher, dass neue Aufgaben korrekt
+        // mit allen notwendigen Informationen erstellt werden können.
+        public UserTask(string id, string name, TaskProcessType type)
+        {
+            Id = id;
+            Name = name;
+            Type = type;
+        }
+    }
+}
